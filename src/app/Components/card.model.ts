d9e051741c7656e1583db91b43123bc779eb32ef
Ezgi Kütük <ezgi.kutuk@derinarge.com>
2020-01-08 16:54:44 +0300

    card ve list kısımları blog haline getirildi

---
+++ src/app/Components/card.model.ts
@@ -0,0 +1,6 @@
+export interface Yemek {
+     title: string;
+     description: string;
+     tag: string;
+     date: string;
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './Components/card/card.component';
+import { ListComponent } from './Components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
